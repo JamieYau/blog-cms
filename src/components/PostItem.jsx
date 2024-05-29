cb30e9987230726be27f325b9fd01d1e7c06c977
JamieYau <jamie.yau2002@me.com>
2024-05-29 01:21:26 +0100

    Refactor PostItem to use ChakraUI LinkBox for semantics

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -11,12 +11,14 @@ import {
   Flex,
   HStack,
   Heading,
+  LinkBox,
+  LinkOverlay,
   Text,
   useDisclosure,
 } from "@chakra-ui/react";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import DeleteModal from "./DeleteModal";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 // Utility function to strip HTML tags and extract text content
 const stripHtmlTags = (html) => {
@@ -25,7 +27,6 @@ const stripHtmlTags = (html) => {
 };
 
 export default function PostItem({ post, onDelete }) {
-  const navigate = useNavigate();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const formattedDate = new Date(post.createdAt).toLocaleDateString();
   const truncatedContent =
@@ -38,35 +39,28 @@ export default function PostItem({ post, onDelete }) {
     onClose();
   };
 
-  const handleCardClick = () => {
-    navigate(`/posts/${post._id}`);
-  };
-
-  const handleOpenModal = (e) => {
-    e.stopPropagation();
-    onOpen();
-  };
-
   return (
-    <Card
-      onClick={handleCardClick}
+    <LinkBox
+      as={Card}
       borderTop="8px"
       borderColor="teal.400"
       bg="white"
       cursor="pointer"
     >
-      <CardHeader>
-        <Flex gap="1em">
-          <Avatar size="md" mr="1em" name={post.author}></Avatar>
-          <Box>
-            <Heading as="h3" size="sm">
-              {post.title}
-            </Heading>
-            <Text>by {post.author}</Text>
-            <Text>{formattedDate}</Text>
-          </Box>
-        </Flex>
-      </CardHeader>
+      <LinkOverlay as={NavLink} to={`/posts/${post._id}`}>
+        <CardHeader>
+          <Flex gap="1em">
+            <Avatar size="md" mr="1em" name={post.author}></Avatar>
+            <Box>
+              <Heading as="h3" size="sm">
+                {post.title}
+              </Heading>
+              <Text>by {post.author}</Text>
+              <Text>{formattedDate}</Text>
+            </Box>
+          </Flex>
+        </CardHeader>
+      </LinkOverlay>
       <CardBody color="gray.500" pt="0">
         <Text>{truncatedContent}</Text>
       </CardBody>
@@ -78,21 +72,16 @@ export default function PostItem({ post, onDelete }) {
             variant="ghost"
             as={NavLink}
             to={`/posts/${post._id}/edit`}
-            onClick={(e) => e.stopPropagation()}
           >
             Edit
           </Button>
-          <Button
-            onClick={(e) => handleOpenModal(e)}
-            leftIcon={<DeleteIcon />}
-            variant="ghost"
-          >
+          <Button onClick={onOpen} leftIcon={<DeleteIcon />} variant="ghost">
             Delete
           </Button>
         </HStack>
       </CardFooter>
       <DeleteModal isOpen={isOpen} onClose={onClose} onDelete={handleDelete} />
-    </Card>
+    </LinkBox>
   );
 }
 
