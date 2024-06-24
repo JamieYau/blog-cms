04eefae2bc649d3447b60d4b15a4baaa91708941
JamieYau <jamie.yau2002@me.com>
2024-06-24 19:49:59 +0100

    Add refresh token chekcs

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,7 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
-import { login as apiLogin } from "../api";
+import { jwtDecode } from "jwt-decode";
+import { login as apiLogin, refreshToken as apiRefreshToken } from "../api";
 
 export const AuthContext = createContext();
 
@@ -9,16 +10,44 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("user")) || null
   );
-  const [token, setToken] = useState(localStorage.getItem("token") || "");
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem("accessToken") || ""
+  );
   const navigate = useNavigate();
+
+  // Function to check accessToken expiration and refresh it if necessary
+  const checkTokenExpiration = async () => {
+    if (accessToken) {
+      const decodedToken = jwtDecode(accessToken);
+      const currentTime = Date.now() / 1000;
+      // If the accessToken has expired
+      if (decodedToken.exp < currentTime) {
+        try {
+          const response = await apiRefreshToken();
+          setAccessToken(response.accessToken);
+          localStorage.setItem("accessToken", response.accessToken);
+        } catch (error) {
+          console.error("Failed to refresh accessToken", error);
+          alert("session timed out");
+          logout();
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Set an interval to check token expiration periodically
+    const interval = setInterval(checkTokenExpiration, 5 * 60 * 1000); // Check every 5 minutes
+    return () => clearInterval(interval); // Clean up the interval on component unmount
+  }, [accessToken]);
 
   const login = async (username, password) => {
     try {
       const response = await apiLogin(username, password); // Use the login function from api.js
       setUser({ username });
-      setToken(response.token);
+      setAccessToken(response.accessToken);
       localStorage.setItem("user", JSON.stringify({ username }));
-      localStorage.setItem("token", response.token);
+      localStorage.setItem("accessToken", response.accessToken);
       navigate("/", { replace: true });
     } catch (error) {
       console.error("Login failed", error);
@@ -28,14 +57,14 @@ const AuthProvider = ({ children }) => {
 
   const logout = () => {
     setUser(null);
-    setToken("");
+    setAccessToken("");
     localStorage.removeItem("user");
-    localStorage.removeItem("token");
+    localStorage.removeItem("accessToken");
     navigate("/");
   };
 
   return (
-    <AuthContext.Provider value={{ user, token, login, logout }}>
+    <AuthContext.Provider value={{ user, accessToken, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
